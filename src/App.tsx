d85b8aab7d93a2b4374b7f3e8355b54eb87716ab
youjiaqi <2430284055@qq.com>
2023-06-19 17:20:49 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./View/Layout/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
